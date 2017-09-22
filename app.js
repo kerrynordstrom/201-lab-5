d49d85d78bd37ad41998f769af08bed378369cbf
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-09-22 13:54:42 -0700

    resubmitting

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function sum(a,b){ //eslint-disable-line
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
   return [theSum, message];
 }
+
+//Resubmitting to branch1
 
 // TODO: Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
